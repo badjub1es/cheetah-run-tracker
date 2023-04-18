ccc23e8e8778148822404375d4d401655188e1c3
anthony <theanthonyarellano@gmail.com>
2023-04-17 22:00:20 -0400

    add greeting

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { getSession, type GetSessionParams } from "next-auth/react";
+import { getSession, useSession, type GetSessionParams } from "next-auth/react";
 import AddShoeModalContainer from "./components/AddShoeModalContainer";
 import AddWorkoutModalContainer from "./components/AddWorkoutModalContainer";
 import ApplicationContainer from "../../components/Containers/ApplicationContainer";
@@ -8,7 +8,21 @@ import ApplicationContainer from "../../components/Containers/ApplicationContain
 const Profile: NextPage = () => {
     const [workoutModalIsOpen, setWorkoutModalIsOpen] = useState(false);
     const [shoeModalIsOpen, setShoeModalIsOpen] = useState(false);
+    const [clear, setClear] = useState(false);
+    const [finalClear, setFinalClear] = useState(false);
 
+    const session = useSession();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setClear(true);
+        }, 2000);
+
+        const timeoutTwo = setTimeout(() => {
+            setFinalClear(true);
+        }, 2500);
+
+    }, []);
     // const handleOpenWorkoutModal = () => {
     //     setWorkoutModalIsOpen((prev) => !prev);
     // };
@@ -19,6 +33,9 @@ const Profile: NextPage = () => {
 
     return (
         <ApplicationContainer>
+            <h1 className={`text-3xl font-extrabold tracking-tight ${clear ? 'text-transparent' : 'text-white'} transition ease-in-out delay-150 duration-500`}>
+                {session.data?.user?.name && !finalClear && `Hello, ${session.data?.user?.name}!`}
+            </h1>
             {/* <div onClick={handleOpenWorkoutModal}>Add Workout</div> */}
             {/* <div onClick={handleOpenShoeModal}>Add Shoe</div> */}
             {workoutModalIsOpen && <AddWorkoutModalContainer />}
