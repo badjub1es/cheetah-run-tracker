c03d2c43c50bdb1950e2ac8c7d766e72b390524a
Anthony Arellano <theanthonyarellano@gmail.com>
2023-07-20 20:32:32 -0400

    add animation for profile content

--- src/pages/profile/components/UserProfile/UserProfileContent/index.tsx
+++ src/pages/profile/components/UserProfile/UserProfileContent/index.tsx
@@ -6,11 +6,15 @@ const UserProfileContent: React.FC = () => {
   useEffect(() => {
     setTimeout(() => {
       setAnimate(true);
-    }, 600);
+    }, 500);
   }, []);
 
   return (
-    <div className="flex h-[50vh] min-w-[48rem] max-w-6xl shrink-0 rounded-b-3xl bg-neutral-200/30 shadow-md">
+    <div
+      className={`flex h-[50vh] min-w-[48rem] max-w-6xl shrink-0 transform rounded-b-3xl bg-neutral-200/30 shadow-md transition-transform duration-500 ${
+        animate ? "scale-100 opacity-100" : "scale-0 opacity-0"
+      }`}
+    >
       content
     </div>
   );
