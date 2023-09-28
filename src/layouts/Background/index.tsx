18cc4e76e90a43543d5e149e4593e89b2544ff98
Anthony Arellano <badjubies@MacBook-Air.fios-router.home>
2023-09-27 21:45:13 -0400

    set up tailwind formatter on save and format file

--- src/layouts/Background/index.tsx
+++ src/layouts/Background/index.tsx
@@ -2,7 +2,7 @@ import React, { type PropsWithChildren } from "react";
 
 const Background: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-asphalt flex h-screen w-screen items-start justify-center">
+    <div className="flex h-screen w-screen items-start justify-center bg-asphalt ">
       {children}
     </div>
   );
