e14d63510254f7b374dc799d0480e84810f8259b
badjub1es <theanthonyarellano@gmail.com>
2023-09-28 21:56:06 -0400

    update styling for discord login component

--- src/components/DiscordSignIn/index.tsx
+++ src/components/DiscordSignIn/index.tsx
@@ -12,7 +12,7 @@ const DiscordSignIn: React.FC<DiscordSignInProps> = ({ width, height }) => (
   <button onClick={() => signInRedirect(AuthProvider.DISCORD)}>
     <div
       style={{ width: `${width}px` }}
-      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[14px] text-base max-w-[${width}px] h-[${height}px] text-white`}
+      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[13px] text-base max-w-[${width}px] h-[${height}px] text-white`}
     >
       <Image
         alt="discord-logo"
@@ -20,7 +20,7 @@ const DiscordSignIn: React.FC<DiscordSignInProps> = ({ width, height }) => (
         width={23}
         height={23}
       />
-      <p>Sign in with Discord</p>
+      <p className="flex-nowrap">Sign in with Discord</p>
     </div>
   </button>
 );
