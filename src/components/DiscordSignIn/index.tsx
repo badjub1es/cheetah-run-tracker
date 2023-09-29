c84df4d3a0d8c3adf311d3901a28b635e624783c
badjub1es <theanthonyarellano@gmail.com>
2023-09-28 21:48:07 -0400

    try max width

--- src/components/DiscordSignIn/index.tsx
+++ src/components/DiscordSignIn/index.tsx
@@ -11,7 +11,7 @@ interface DiscordSignInProps {
 const DiscordSignIn: React.FC<DiscordSignInProps> = ({ width, height }) => (
   <button onClick={() => signInRedirect(AuthProvider.DISCORD)}>
     <div
-      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[14px] text-base w-[${width}px] h-[${height}px] text-white`}
+      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[14px] text-base w-[${width}px] max-w-[${width}px] h-[${height}px] text-white`}
     >
       <Image
         alt="discord-logo"
