c4e27b7ae128cd73f7260c31ce4ead458fb912cc
badjub1es <theanthonyarellano@gmail.com>
2023-09-29 23:36:01 -0400

    change text size

--- src/components/DiscordSignIn/index.tsx
+++ src/components/DiscordSignIn/index.tsx
@@ -12,7 +12,7 @@ const DiscordSignIn: React.FC<DiscordSignInProps> = ({ width, height }) => (
   <button onClick={() => signInRedirect(AuthProvider.DISCORD)}>
     <div
       style={{ width: `${width}px` }}
-      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[14px] text-base shadow max-w-[${width}px] h-[${height}px] text-white`}
+      className={`flex gap-[19px] overflow-hidden rounded-sm bg-blurple px-3 py-[9px] text-[14px] shadow max-w-[${width}px] h-[${height}px] text-white`}
     >
       <Image
         alt="discord-logo"
--- src/components/GoogleSignIn/index.tsx
+++ src/components/GoogleSignIn/index.tsx
@@ -15,7 +15,7 @@ const GoogleSignIn: React.FC<GoogleSignInProps> = ({
   <button onClick={() => signInRedirect(AuthProvider.GOOGLE)}>
     <div
       style={{ width: `${width}px` }}
-      className={`flex gap-[19px] overflow-hidden rounded-sm bg-white px-3 py-[9px] text-[14px] text-base shadow max-w-[${width}px] h-[${height}px] text-[#2d2d2d]`}
+      className={`flex gap-[19px] overflow-hidden rounded-sm bg-white px-3 py-[9px] text-[14px] shadow max-w-[${width}px] h-[${height}px] text-[#2d2d2d]`}
     >
       <Image alt="google-logo" src={"/googleLogo.png"} width={23} height={23} />
       <p className="flex-nowrap">Sign in with Google</p>
