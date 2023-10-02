f6ea222ec45209f027fcf4e5cc9038b1a06ebfeb
badjub1es <theanthonyarellano@gmail.com>
2023-10-01 20:00:30 -0400

    add cheetah logo

--- src/components/CheetahLogo/index.tsx
+++ src/components/CheetahLogo/index.tsx
@@ -1,16 +1,25 @@
-import { useRouter } from "next/router";
+import Image from "next/image";
 import EnRoutePaths from "@customTypes/routes/EnRoutePaths";
+import { useRouter } from "next/router";
 
 const CheetahLogo: React.FC = ({}) => {
   const router = useRouter();
 
   return (
-    <h1
-      onClick={() => router.push(EnRoutePaths.profile)}
-      className="text-1xl cursor-pointer align-middle font-extrabold tracking-tight text-white sm:text-[2rem]"
-    >
-      cheetah
-    </h1>
+    <>
+      <h1
+        onClick={() => router.push(EnRoutePaths.profile)}
+        className="text-1xl cursor-pointer align-middle font-extrabold tracking-tight text-white sm:text-[2rem]"
+      >
+        cheetah
+      </h1>
+      <Image
+        alt="cheetah-logo"
+        src={"/cheetahLogoBgRemoved.png"}
+        height={25}
+        width={25}
+      />
+    </>
   );
 };
 
