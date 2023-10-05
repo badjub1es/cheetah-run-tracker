3a3d581b05da60157a188421389fbe69ce2c0940
badjub1es <theanthonyarellano@gmail.com>
2023-10-04 20:45:17 -0400

    add styling to background image so it will not grow or repeat upon page resize

--- src/layouts/Background/index.tsx
+++ src/layouts/Background/index.tsx
@@ -2,7 +2,7 @@ import React, { type PropsWithChildren } from "react";
 
 const Background: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-[url('./cheetahBackground.png')] px-[25vw] ">
+    <div className="flex min-h-screen w-screen items-center justify-center bg-landing-image bg-cover bg-fixed bg-center bg-no-repeat px-[25vw] ">
       {children}
     </div>
   );
