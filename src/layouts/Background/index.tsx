da7f470c605c97f09ab8fe14f151a53202a9465a
badjub1es <theanthonyarellano@gmail.com>
2023-10-04 20:30:14 -0400

    attempt to add background image

--- src/layouts/Background/index.tsx
+++ src/layouts/Background/index.tsx
@@ -2,7 +2,7 @@ import React, { type PropsWithChildren } from "react";
 
 const Background: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-asphalt px-[25vw] ">
+    <div className="flex h-screen w-screen items-center justify-center bg-[url('./cheetahBackground.png')] px-[25vw] ">
       {children}
     </div>
   );
