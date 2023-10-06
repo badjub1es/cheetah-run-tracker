8d6276422533f43ea784236b1253786c040d95fe
badjub1es <theanthonyarellano@gmail.com>
2023-10-05 21:22:50 -0400

    add styling for menu button

--- src/layouts/Header/components/MenuDropDown/index.tsx
+++ src/layouts/Header/components/MenuDropDown/index.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import { useSession } from "next-auth/react";
+import UserProfileImage from "@components/UserProfileImage";
 
 interface MenuDropDownProps {}
 
 const MenuDropDown: React.FC<MenuDropDownProps> = () => {
+  const session = useSession();
   return (
-    <div className="h-max w-max rounded-md border-2 border-solid border-white bg-frost bg-white p-2">
-      something
+    <div className="h-max w-max cursor-pointer rounded-md border-2 border-solid border-white bg-frost bg-white p-2">
+      <UserProfileImage
+        src={session.data?.user?.image || ""}
+        height={30}
+        width={30}
+        borderStyle="border-2 border-solid border-white"
+      />
     </div>
   );
 };
