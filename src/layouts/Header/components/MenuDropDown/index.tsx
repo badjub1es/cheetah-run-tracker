e47d8a2687f52e4c1d4248a979f1a840e3bd74fa
badjub1es <theanthonyarellano@gmail.com>
2023-10-05 22:49:49 -0400

    alter image size

--- src/layouts/Header/components/MenuDropDown/index.tsx
+++ src/layouts/Header/components/MenuDropDown/index.tsx
@@ -7,8 +7,8 @@ const MenuDropDown: React.FC = () => {
     <div className="h-max w-max cursor-pointer rounded-md border-2 border-solid border-white bg-frost bg-white p-2">
       <UserProfileImage
         src={session.data?.user?.image || ""}
-        height={30}
-        width={30}
+        height={27}
+        width={27}
         borderStyle="border-2 border-solid border-white"
       />
     </div>
