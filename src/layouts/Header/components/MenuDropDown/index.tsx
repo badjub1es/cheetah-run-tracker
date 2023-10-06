a504d8aa3cee619cbc31116b4c68802bf831cba4
badjub1es <theanthonyarellano@gmail.com>
2023-10-05 21:09:18 -0400

    add initial styling for component

--- src/layouts/Header/components/MenuDropDown/index.tsx
+++ src/layouts/Header/components/MenuDropDown/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 interface MenuDropDownProps {}
 
 const MenuDropDown: React.FC<MenuDropDownProps> = () => {
-  return <></>;
+  return (
+    <div className="h-max w-max rounded-md border-2 border-solid border-white bg-frost bg-white p-2">
+      something
+    </div>
+  );
 };
 
 export default MenuDropDown;
