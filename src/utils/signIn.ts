04d8819fbaa85950788ed1574d77df6c098222a3
badjub1es <theanthonyarellano@gmail.com>
2023-10-05 22:56:03 -0400

    redirect to /home on login

--- src/utils/signIn.ts
+++ src/utils/signIn.ts
@@ -2,5 +2,5 @@ import { signIn } from "next-auth/react";
 import type { AuthProvider } from "@customTypes/authProviders";
 
 export const signInRedirect = (provider: AuthProvider) => {
-    signIn(provider, { callbackUrl: "/profile" });
+    signIn(provider, { callbackUrl: "/home" });
 };
