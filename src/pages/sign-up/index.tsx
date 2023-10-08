75162f9c15535ce6af5bb2a5498563aa7b23f288
badjub1es <theanthonyarellano@gmail.com>
2023-10-08 18:30:36 -0400

    update test code to use env url

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -3,16 +3,19 @@ import { signIn } from "next-auth/react";
 
 const SignUp: NextPage = () => {
   const handleClick = async () => {
-    const res = await fetch("http://localhost:3000/api/user/create", {
-      method: "POST",
-      body: JSON.stringify({
-        email: "some_email3@gmail.com",
-        password: "sTr!O1ngpass",
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const res = await fetch(
+      `http://${process.env.NEXTAUTH_URL}/api/user/create`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: "some_email3@gmail.com",
+          password: "sTr!O1ngpass",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     if (res.ok) {
     } else {
