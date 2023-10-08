ec1089c4a2e9b6adad367a804378234c9e425ffa
badjub1es <theanthonyarellano@gmail.com>
2023-10-07 23:04:41 -0400

    create sign up page

---
+++ src/pages/sign-up/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const SignUp: NextPage = () => {
+  return (
+    <div className="flex h-auto flex-col justify-center rounded-3xl bg-neutral-200/30 shadow-md">
+      <div className="flex flex-col items-center justify-center gap-5 px-10 py-16">
+        Sign up page
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
