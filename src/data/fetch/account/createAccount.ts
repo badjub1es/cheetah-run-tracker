10d61d73620b24d70c5cd9b715d1ef8e6213f90c
badjub1es <theanthonyarellano@gmail.com>
2023-10-11 20:41:01 -0400

    create data folder to house fetch, start styling sign up page

---
+++ src/data/fetch/account/createAccount.ts
@@ -0,0 +1,24 @@
+import { signIn } from "next-auth/react";
+
+export const createAccount = async (email: string, password: string) => {
+    const res = await fetch(`${process.env.BASE_URL}/api/user/create`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (res.ok) {
+      signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/home",
+      });
+    } else {
+      // display error messaging
+    }
+  };
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import isEmail from "validator/lib/isEmail";
 import type { NextPage } from "next";
 import { signIn } from "next-auth/react";
+import { createAccount } from "data/fetch";
 import { validateStrongPassword } from "utils/validateStrongPassword";
 
 const SignUp: NextPage = () => {
@@ -18,29 +19,6 @@ const SignUp: NextPage = () => {
     missingPassword: false,
     missingVerifiedPassword: false,
   });
-
-  const handleCreateAccount = async (email: string, password: string) => {
-    const res = await fetch(`${process.env.BASE_URL}/api/user/create`, {
-      method: "POST",
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (res.ok) {
-      signIn("credentials", {
-        email,
-        password,
-        callbackUrl: "/home",
-      });
-    } else {
-      // display error messaging
-    }
-  };
 
   const handleLogin = async (email: string, password: string) => {
     signIn("credentials", {
@@ -79,7 +57,7 @@ const SignUp: NextPage = () => {
   const submitCreate = () => {
     setHasSubmitted(true);
     if (isValidForm) {
-      handleCreateAccount(email, password);
+      createAccount(email, password);
     }
   };
 
@@ -97,30 +75,68 @@ const SignUp: NextPage = () => {
 
   return (
     <div className="flex h-auto flex-col justify-center rounded-3xl bg-neutral-200/30 shadow-md">
-      <div className="flex flex-col items-center justify-center gap-5 px-10 py-16">
-        Sign up page
+      <div className="flex flex-col items-center justify-center gap-5 px-10 py-10">
+        <h1 className="font-extrabold text-white">Sign up</h1>
+        <div className="flex flex-col gap-3">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email
+          </label>
+          <input
+            type="text"
+            id="email"
+            onChange={handleEmailChange}
+            className="block w-full rounded-lg border border-frost bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-frost focus:ring-frost dark:border-frost dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-frost dark:focus:ring-frost"
+            placeholder="Enter email"
+            required
+          />
+          {hasSubmitted && errors?.invalidEmail && (
+            <p className="text-red-700">Invalid Email</p>
+          )}
+          <label
+            htmlFor="password"
+            className=" block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            onChange={handlePasswordChange}
+            className="block w-full rounded-lg border border-frost bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-frost focus:ring-frost dark:border-frost dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-frost dark:focus:ring-frost"
+            placeholder="Enter password"
+            required
+          />
+          {hasSubmitted && errors?.invalidPassword && (
+            <p className="text-red-700">Password sucks</p>
+          )}
+          <label
+            htmlFor="verify-password"
+            className="block text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Verify password
+          </label>
+          <input
+            type="password"
+            id="verify-password"
+            onChange={handleVerifyPasswordChange}
+            className="block w-full rounded-lg border border-frost bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-frost focus:ring-frost dark:border-frost dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-frost dark:focus:ring-frost"
+            placeholder="Verify password"
+            required
+          />
+          {hasSubmitted && errors?.mismatchPassword && (
+            <p className="text-red-700">Passwords do not match</p>
+          )}
+        </div>
+        <button
+          className="rounded-lg border border-white p-2.5 text-white"
+          onClick={submitCreate}
+        >
+          Create Account
+        </button>
       </div>
-      <div className="flex flex-col gap-3">
-        <input placeholder="Enter email" onChange={handleEmailChange} />
-        {hasSubmitted && errors?.invalidEmail && (
-          <p className="text-red-700">Invalid Email</p>
-        )}
-        <input placeholder="Enter password" onChange={handlePasswordChange} />
-        {hasSubmitted && errors?.invalidPassword && (
-          <p className="text-red-700">Password sucks</p>
-        )}
-        <input
-          placeholder="Verify password"
-          onChange={handleVerifyPasswordChange}
-        />
-        {hasSubmitted && errors?.mismatchPassword && (
-          <p className="text-red-700">Passwords do not match</p>
-        )}
-      </div>
-      <button onClick={submitCreate}>Create Account</button>
-
-      {/* <button onClick={handleClick}>fake sign up</button> */}
-      {/* <button onClick={handleLogin}>fake log in</button> */}
     </div>
   );
 };
