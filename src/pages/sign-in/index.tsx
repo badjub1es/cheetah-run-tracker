96efd915113de9644bdf46d90fee9fe51a1e6178
anthony <theanthonyarellano@gmail.com>
2023-10-18 21:04:55 -0400

    display error message on user sign in error

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -9,6 +9,7 @@ const SignIn: NextPage = () => {
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(false);
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const email = event.target.value;
@@ -22,9 +23,16 @@ const SignIn: NextPage = () => {
 
   const handleLogin = (email: string, password: string) => {
     signIn("credentials", {
+      redirect: false,
       email,
       password,
       callbackUrl: "/home",
+    }).then((res) => {
+      if (res?.ok) {
+        router.push("/home");
+      } else {
+        setError(true);
+      }
     });
   };
 
@@ -32,6 +40,7 @@ const SignIn: NextPage = () => {
     <div className="flex h-auto flex-col justify-center rounded-3xl bg-neutral-200/30 shadow-md">
       <div className="flex flex-col items-center justify-center gap-5 px-10 py-10">
         <h1 className="font-extrabold text-white">Welcome back</h1>
+        {error && <p className="text-red-500">Invalid credentials</p>}
         <div className="flex flex-col gap-3">
           <label
             htmlFor="email"
