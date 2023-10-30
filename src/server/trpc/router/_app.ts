395501b0c5a1dedd3eaeba14f9e18d5372a4229d
anthony <theanthonyarellano@gmail.com>
2023-10-29 21:39:24 -0400

    remove exampleRouter from trpc app router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
 import { shoesRouter } from "./shoes";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
-  shoes: shoesRouter
+  shoes: shoesRouter,
 });
 
 // export type definition of API
